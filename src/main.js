3e20befdc0634fdfcb38a865d02e8febca6f382e
LesiaPrudyus <lesia.prudyus@p2h.com>
2021-01-25 21:02:56 +0300

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routes'
+import Vuex from 'vuex'
+import store from './store'
 
 import './assets/scss/main.scss'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
+  store,
   render: h => h(App),
   el: '#app'
 })
